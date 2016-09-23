b298a1ba215249432f8b6bd253fc059613062fd0
Valentin Richard <valentin.richard@utopix.ch>
2016-09-23 16:11:59 +0200

    rename index to ng2-component-injector

--- ng2-component-injector.js
+++ ng2-component-injector.js
@@ -3,4 +3,4 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require('./lib/component.injector.service'));
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=ng2-component-injector.js.map
